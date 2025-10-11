837a97ce88c4f0eed55dcd3b3917a977bad23b73
Harsh31105 <harshnirajsingh@gmail.com>
2025-10-11 19:57:04 -0400

    chore: Added & re-ordered entries.

--- src/Components/AboutComponents/TeamShowcaseComponents/TeamConstants.js
+++ src/Components/AboutComponents/TeamShowcaseComponents/TeamConstants.js
@@ -34,44 +34,67 @@ import mikaTums from "../../../img/eboard_photos_F25/mika_tums.jpeg"
 import zachGuan from "../../../img/eboard_photos_F25/zach_guan.jpeg"
 import aaratrikaUpadhyay from "../../../img/eboard_photos_F25/aaratrika_upadhyay.jpeg";
 import nateChoe from "../../../img/eboard_photos_F25/nate_choe.jpeg"
+import eitanBerenfeld from "../../../img/eboard_photos_F25/eitan_berenfeld.jpeg"
+import tehyaKetvoravit from "../../../img/eboard_photos_F25/tehya_ketvoravit.jpeg"
 
 export const TeamMember = [
+    // E-Board
   { key: "arnavMishra-31", headshot: arnavMishra, name: "Arnav Mishra", position: "President", team: "E-Board", linkedin: "https://www.linkedin.com/in/arnav-mishra-1960001b1/", alt: "" },
   { key: "nicoleSchubert-14", headshot: nicoleSchubert, name: "Nicole Schubert", position: "Vice-President", team: "E-Board", linkedin: "https://www.linkedin.com/in/nicole-schubert1/", alt: "" },
   { key: "rodrigoAldrey-32", headshot: rodrigoAldrey, name: "Rodrigo Aldrey", position: "Director of Technology", team: "E-Board", linkedin: "https://www.linkedin.com/in/rodrigo-aldrey-558808244/", alt: "" },
-  { key: "ericaZheng-5", headshot: ericaZheng, name: "Erica Zheng", position: "Marketing & Events Director", team: "E-Board", linkedin: "https://www.linkedin.com/in/zhengerica/", alt: "" },
-  { key: "xavierYu-20", headshot: xavierYu, name: "Xavier Yu", position: "Director of Research", team: "E-Board", linkedin: "https://www.linkedin.com/in/xavier-yu/", alt: "" },
-  { key: "aileenRuan-1", headshot: unknown, name: "Aileen Ruan", position: "Events Coordinator", team: "Events Team", linkedin: "https://www.linkedin.com/in/aileenruan/", alt: "" },
-  { key: "arjunSikka-2", headshot: arjunSikka, name: "Arjun Sikka", position: "", team: "Tech Team", linkedin: "https://www.linkedin.com/in/arjunsikka52/", alt: "" },
-  { key: "atulyaKathuria-3", headshot: atulyaKathuria, name: "Atulya Kathuria", position: "Outreach Coordinator", team: "Outreach Team", linkedin: "https://www.linkedin.com/in/atulyakathuria/", alt: "" },
-  { key: "aidanKelly-4", headshot: aidanKelly, name: "Aidan Kelly", position: "", team: "Quant Team", linkedin: "https://www.linkedin.com/in/aidan-j-kelly/", alt: "" },
-  { key: "madelineShih-6", headshot: madelineShih, name: "Madeline Shih", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/madeline-shih-9b4a88269/", alt: "" },
-  { key: "georgeFoster-7", headshot: georgeFoster, name: "George Foster", position: "", team: "Quant Team", linkedin: "https://www.linkedin.com/in/foster22george/", alt: "" },
-  { key: "edisonKwok-8", headshot: edisonKwok, name: "Edison Kwok", position: "", team: "Tech Team", linkedin: "https://www.linkedin.com/in/edison-kwok8/", alt: "" },
-  { key: "gordonBie-9", headshot: gordonBie, name: "Gordon Bie", position: "", team: "Marketing Team", linkedin: "https://www.linkedin.com/in/gordon-bie/", alt: "" },
-  { key: "vedNaykude-10", headshot: vedNaykude, name: "Ved Naykude", position: "", team: "Quant Team", linkedin: "https://www.linkedin.com/in/vedn123/", alt: "" },
-  { key: "anyaSolanki-11", headshot: anyaSolanki, name: "Anya Solanki", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/anyasolanki/", alt: "" },
-  { key: "eitanBerenfeld-12", headshot: unknown, name: "Eitan Berenfeld", position: "", team: "Quant Team", linkedin: "https://www.linkedin.com/in/eitanberenfeld/", alt: "" },
-  { key: "cameronRyan-13", headshot: cameronRyan, name: "Cameron Ryan", position: "", team: "Quant Team", linkedin: "https://www.linkedin.com/in/cameron--ryan/", alt: "" },
-  { key: "riyaMohandas-15", headshot: unknown, name: "Riya Mohandas", position: "", team: "Tech Team", linkedin: "", alt: "" },
-  { key: "luluYu-16", headshot: luluYu, name: "Lulu Yu", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/jas-lulu-yu/", alt: "" },
   { key: "oliviaYeung-17", headshot: oliviaYeung, name: "Olivia Yeung", position: "Treasurer", team: "E-Board", linkedin: "https://www.linkedin.com/in/olivia-yeung/", alt: "" },
-  { key: "kristinaSaavedra-18", headshot: kristinaSaavedra, name: "Kristina Saavedra", position: "Events Coordinator", team: "Events Team", linkedin: "https://www.linkedin.com/in/kristina-saavedra-a89b192a3/", alt: "" },
-  { key: "harshSingh-19", headshot: harshSingh, name: "Harsh Singh", position: "", team: "Tech Team", linkedin: "https://www.linkedin.com/in/harsh-singh973/", alt: "" },
-  { key: "nateHarris-21", headshot: nateHarris, name: "Nate Harris", position: "Outreach Coordinator", team: "Outreach Team", linkedin: "https://www.linkedin.com/in/harris-nate/", alt: "" },
-  { key: "nafisaTasnia-22", headshot: nafisaTasnia, name: "Nafisa Tasnia", position: "", team: "Quant Team", linkedin: "https://www.linkedin.com/in/nafisa-tasnia-73a0232a6/", alt: "" },
-  { key: "wilsonChang-23", headshot: wilsonChang, name: "Wilson Chang", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/wilson-chang-4744262a4/", alt: "" },
-  { key: "aryanSinha-24", headshot: aryanSinha, name: "Aryan Sinha", position: "", team: "Quant Team", linkedin: "https://www.linkedin.com/in/aryan-sinha-a0b294242/", alt: "" },
-  { key: "connorTessaro-25", headshot: connorTessaro, name: "Connor Tessaro", position: "", team: "Tech Team", linkedin: "https://www.linkedin.com/in/connortessaro/", alt: "" },
-  { key: "riyaAnanthanpillai-26", headshot: riyaAnanthanpillai, name: "Riya Ananthanpillai", position: "Outreach Coordinator", team: "Outreach Team", linkedin: "https://www.linkedin.com/in/riya-ananthanpillai/", alt: "" },
-  { key: "abigailEng-27", headshot: abigailEng, name: "Abigail Eng", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/abigail-eng-12a00b273/", alt: "" },
-  { key: "shireenKumar-28", headshot: shireenKumar, name: "Shireen Kumar", position: "", team: "Tech Team", linkedin: "https://www.linkedin.com/in/shireen-kumar-387132210/", alt: "" },
+  { key: "xavierYu-20", headshot: xavierYu, name: "Xavier Yu", position: "Director of Research", team: "E-Board", linkedin: "https://www.linkedin.com/in/xavier-yu/", alt: "" },
+  { key: "ericaZheng-5", headshot: ericaZheng, name: "Erica Zheng", position: "Marketing & Events Director", team: "E-Board", linkedin: "https://www.linkedin.com/in/zhengerica/", alt: "" },
+
+    // Events Team
   { key: "laithBuheiry-29", headshot: laithBuheiry, name: "Laith Buheiry", position: "Events Coordinator", team: "Events Team", linkedin: "https://www.linkedin.com/in/laith-buheiry/", alt: "" },
-  { key: "chelseaKwan-30", headshot: chelseaKwan, name: "Chelsea Kwan", position: "Project Manager", team: "Consulting Team", linkedin: "https://www.linkedin.com/in/chelseagkwan/", alt: "" },
-  { key: "cindychen-33", headshot: cindyChen, name: "Cindy Chen", position: "", team: "Tech Team", linkedin: "https://www.linkedin.com/in/cindy-chen-643ab1287/", alt: "" },
+  { key: "aileenRuan-1", headshot: unknown, name: "Aileen Ruan", position: "Events Coordinator", team: "Events Team", linkedin: "https://www.linkedin.com/in/aileenruan/", alt: "" },
+  { key: "kristinaSaavedra-18", headshot: kristinaSaavedra, name: "Kristina Saavedra", position: "Events Coordinator", team: "Events Team", linkedin: "https://www.linkedin.com/in/kristina-saavedra-a89b192a3/", alt: "" },
+
+    // Tech Team
+  { key: "cindychen-33", headshot: cindyChen, name: "Cindy Chen", position: "Software Developer", team: "Tech Team", linkedin: "https://www.linkedin.com/in/cindy-chen-643ab1287/", alt: "" },
+  { key: "shireenKumar-28", headshot: shireenKumar, name: "Shireen Kumar", position: "Software Developer", team: "Tech Team", linkedin: "https://www.linkedin.com/in/shireen-kumar-387132210/", alt: "" },
+  { key: "edisonKwok-8", headshot: edisonKwok, name: "Edison Kwok", position: "Software Developer", team: "Tech Team", linkedin: "https://www.linkedin.com/in/edison-kwok8/", alt: "" },
+  { key: "riyaMohandas-15", headshot: unknown, name: "Riya Mohandas", position: "Software Developer", team: "Tech Team", linkedin: "", alt: "" },
+  { key: "arjunSikka-2", headshot: arjunSikka, name: "Arjun Sikka", position: "Software Developer", team: "Tech Team", linkedin: "https://www.linkedin.com/in/arjunsikka52/", alt: "" },
+  { key: "harshSingh-19", headshot: harshSingh, name: "Harsh Singh", position: "Software Developer", team: "Tech Team", linkedin: "https://www.linkedin.com/in/harsh-singh973/", alt: "" },
+  { key: "connorTessaro-25", headshot: connorTessaro, name: "Connor Tessaro", position: "Software Developer", team: "Tech Team", linkedin: "https://www.linkedin.com/in/connortessaro/", alt: "" },
+
+  // Outreach Team
+  { key: "atulyaKathuria-3", headshot: atulyaKathuria, name: "Atulya Kathuria", position: "Outreach Coordinator", team: "Outreach Team", linkedin: "https://www.linkedin.com/in/atulyakathuria/", alt: "" },
+  { key: "nateHarris-21", headshot: nateHarris, name: "Nate Harris", position: "Outreach Coordinator", team: "Outreach Team", linkedin: "https://www.linkedin.com/in/harris-nate/", alt: "" },
+  { key: "riyaAnanthanpillai-26", headshot: riyaAnanthanpillai, name: "Riya Ananthanpillai", position: "Outreach Coordinator", team: "Outreach Team", linkedin: "https://www.linkedin.com/in/riya-ananthanpillai/", alt: "" },
+
+  // Quant Team
+  { key: "eitanBerenfeld-12", headshot: eitanBerenfeld, name: "Eitan Berenfeld", position: "Quantitative Analyst", team: "Quant Team", linkedin: "https://www.linkedin.com/in/eitanberenfeld/", alt: "" },
+  { key: "georgeFoster-7", headshot: georgeFoster, name: "George Foster", position: "Quantitative Analyst", team: "Quant Team", linkedin: "https://www.linkedin.com/in/foster22george/", alt: "" },
+  { key: "aidanKelly-4", headshot: aidanKelly, name: "Aidan Kelly", position: "Quantitative Analyst", team: "Quant Team", linkedin: "https://www.linkedin.com/in/aidan-j-kelly/", alt: "" },
+  { key: "vedNaykude-10", headshot: vedNaykude, name: "Ved Naykude", position: "Quantitative Analyst", team: "Quant Team", linkedin: "https://www.linkedin.com/in/vedn123/", alt: "" },
+  { key: "cameronRyan-13", headshot: cameronRyan, name: "Cameron Ryan", position: "Quantitative Analyst", team: "Quant Team", linkedin: "https://www.linkedin.com/in/cameron--ryan/", alt: "" },
+  { key: "aryanSinha-24", headshot: aryanSinha, name: "Aryan Sinha", position: "Quantitative Analyst", team: "Quant Team", linkedin: "https://www.linkedin.com/in/aryan-sinha-a0b294242/", alt: "" },
+  { key: "nafisaTasnia-22", headshot: nafisaTasnia, name: "Nafisa Tasnia", position: "Quantitative Analyst", team: "Quant Team", linkedin: "https://www.linkedin.com/in/nafisa-tasnia-73a0232a6/", alt: "" },
+  { key: "aaratrikaUpadhyay-37", headshot: aaratrikaUpadhyay, name: "Aaratrika Upadhyay", position: "Quantitative Analyst", team: "Quant Team", linkedin: "https://www.linkedin.com/in/aaratrika-upadhyay-8269212b7/", alt: "" },
+
+  // Research Team
+  { key: "wilsonChang-23", headshot: wilsonChang, name: "Wilson Chang", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/wilson-chang-4744262a4/", alt: "" },
+  { key: "abigailEng-27", headshot: abigailEng, name: "Abigail Eng", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/abigail-eng-12a00b273/", alt: "" },
   { key: "aadyapai-34", headshot: aadyaPai, name: "Aadya Pai", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/aadya-pai/", alt: "" },
-  { key: "mikatums-35", headshot: mikaTums, name: "Mika Tums", position: "", team: "Consulting Team", linkedin: "https://www.linkedin.com/in/mika-tums/", alt: "" },
-  { key: "zachguan-36", headshot: zachGuan, name: "Zach Guan", position: "", team: "Consulting Team", linkedin: "https://www.linkedin.com/in/zachguan/", alt: "" },
-  { key: "aaratrikaUpadhyay-37", headshot: aaratrikaUpadhyay, name: "Aaratrika Upadhyay", position: "", team: "Quant Team", linkedin: "https://www.linkedin.com/in/aaratrika-upadhyay-8269212b7/", alt: "" },
+  { key: "madelineShih-6", headshot: madelineShih, name: "Madeline Shih", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/madeline-shih-9b4a88269/", alt: "" },
+  { key: "anyaSolanki-11", headshot: anyaSolanki, name: "Anya Solanki", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/anyasolanki/", alt: "" },
+  { key: "luluYu-16", headshot: luluYu, name: "Lulu Yu", position: "Research Analyst", team: "Research Team", linkedin: "https://www.linkedin.com/in/jas-lulu-yu/", alt: "" },
+
+  // Marketing Team
+  { key: "gordonBie-9", headshot: gordonBie, name: "Gordon Bie", position: "Marketing Coordinator", team: "Marketing Team", linkedin: "https://www.linkedin.com/in/gordon-bie/", alt: "" },
+
+    // Consulting Team
+  { key: "chelseaKwan-30", headshot: chelseaKwan, name: "Chelsea Kwan", position: "Project Manager", team: "Consulting Team", linkedin: "https://www.linkedin.com/in/chelseagkwan/", alt: "" },
+  { key: "tehyaKetvoravit-39", headshot: tehyaKetvoravit, name: "Tehya Ketvoravit", position: "Associate Consultant", team: "Consulting Team", linkedin: "https://www.linkedin.com/in/tayapat-ketvoravit/", alt: "" },
+
   { key: "natechoe-38", headshot: nateChoe, name: "Nate Choe", position: "", team: "Consulting Team", linkedin: "https://www.linkedin.com/in/nathaniel-choe-01a273318/", alt: "" },
+  { key: "zachguan-36", headshot: zachGuan, name: "Zach Guan", position: "", team: "Consulting Team", linkedin: "https://www.linkedin.com/in/zachguan/", alt: "" },
+  { key: "mikatums-35", headshot: mikaTums, name: "Mika Tums", position: "", team: "Consulting Team", linkedin: "https://www.linkedin.com/in/mika-tums/", alt: "" },
+
+
 ];
+
+// TODO: ORDER TEAMS based on Number, Order People based on Seniority vs. Last Name. Add the Remaining People.
